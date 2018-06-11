92f3b6df88f152ca76d51569f94c8212bdf700dc
boboan <boboan@an.com>
2018-06-12 00:46:59 +0800

    init

---
+++ src/main/java/com/mmall/Concurrency/annoaction/ThreadSafe.java
@@ -0,0 +1,16 @@
+package com.mmall.Concurrency.annoaction;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by 香香和臭臭 on 2018/6/12.
+ * 用来标记线程安全的类或者写法
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface ThreadSafe {
+    String value() default "";
+}
